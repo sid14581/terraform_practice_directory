82ca049232170a51ede1c1cec54800177d1fe086
sid14581 <siddhardh.kbv@gmail.com>
2024-06-07 19:21:12 +0530

    added natgw & sgp

---
+++ tf_vpc_setup/security_grp_setup.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "sid_sg" {
+  egress = [{
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    self = false 
+    ipv6_cidr_blocks = [  ]
+    prefix_list_ids = [  ]
+    security_groups = [  ]
+    description = ""
+    
+  }]
+  ingress = [{
+    cidr_blocks = ["0.0.0.0/0" ]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    self = false
+    ipv6_cidr_blocks = [  ]
+    prefix_list_ids = [  ]
+    security_groups = [  ]
+    description = ""
+
+  }]
+  vpc_id = aws_vpc.sid_vpc.id
+  depends_on = [ aws_vpc.sid_vpc ]
+  tags = {
+    Name = "sid_sg"
+  }
+}
