0418d3d8d739fadd69202aa3be8fe6a05d3a45a8
sid14581 <siddhardh.kbv@gmail.com>
2025-04-17 22:04:43 +0530

    minor changes in ec2 & key-pairs,  also changes in security grp

--- provisioners_aws/ec2-instance.tf
+++ provisioners_aws/ec2-instance.tf
@@ -2,17 +2,17 @@ resource "aws_instance" "server" {
   ami                    = "ami-0261755bbcb8c4a84"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.provision_key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.webSg.id]
+  vpc_security_group_ids = [aws_security_group.provision_sgp.id]
   subnet_id              = aws_subnet.provision_sub1.id
 
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("C:\\Users\\<username>\\.ssh\\id_ed25519.pub")
+    private_key = file("C:\\Users\\<user>\\.ssh\\id_ed25519")
     host        = self.public_ip
   }
 
-  
+
   provisioner "file" {
     source      = "app.py"
     destination = "/home/ubuntu/app.py"
--- provisioners_aws/securitygrp.tf
+++ provisioners_aws/securitygrp.tf
@@ -2,22 +2,44 @@ resource "aws_security_group" "provision_sgp" {
   name   = var.sgp_name
   vpc_id = aws_vpc.provision_vpc.id
 
-  ingress = [{ description = "HTTP from VPC"
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] },
-    { description = "SSH"
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"] }]
+  ingress = [
+    {
+      description      = "HTTP from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      self             = false
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+    },
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      self             = false
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+    }
+  ]
 
-  egress = [{ from_port = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }]
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      self             = false
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      description      = ""
+    }
+  ]
 
   tags = {
     Name = "Web-sg"
