dc3e634fa3393390b6d2be9b9d45a72d076586fc
sid14581 <siddhardh.kbv@gmail.com>
2025-04-28 22:45:12 +0530

    change in the ami

--- provisioners_aws/ec2-instance.tf
+++ provisioners_aws/ec2-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "server" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-04f167a56786e4b09"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.provision_key_pair.key_name
   vpc_security_group_ids = [aws_security_group.provision_sgp.id]
@@ -10,6 +10,7 @@ resource "aws_instance" "server" {
     user        = "ubuntu"
     private_key = file("C:\\Users\\<user>\\.ssh\\id_ed25519")
     host        = self.public_ip
+    timeout = "2m"
   }
 
 
@@ -20,12 +21,12 @@ resource "aws_instance" "server" {
 
   provisioner "remote-exec" {
     inline = [
-      "echo 'Hello from the remote instance'",
-      "sudo apt update -y",
-      "sudo apt-get install -y python3-pip",
-      "cd /home/ubuntu",
-      "sudo pip3 install flask",
-      "sudo python3 app.py &",
+        "echo 'Hello from the remote instance'",
+        "sudo apt update -y",
+        "cd /home/ubuntu/",
+        "sudo apt install -y python3-flask",
+        "sudo python3 /home/ubuntu/app.py &"
     ]
   }
+   depends_on = [aws_key_pair.provision_key_pair]
 }
