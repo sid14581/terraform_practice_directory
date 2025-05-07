e7f65a04738ad431916761c6d8d0a5c55d7ef03b
sid14581 <siddhardh.kbv@gmail.com>
2025-05-07 20:21:30 +0530

    files updated

--- ecs-fargate-wordpress-rds-terraform/provider.tf
+++ ecs-fargate-wordpress-rds-terraform/provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-  backend "s3" {
-    bucket         = "ecs-fargate-wordpress-rds-bucket"
-    key            = "ecs-fargate-wordpress-rds/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "ecs-fargate-wordpress-rds-lock"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
