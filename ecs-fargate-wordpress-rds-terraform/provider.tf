8f31a06c4704fb32e319fb069ebcb6d4869ffd81
sid14581 <siddhardh.kbv@gmail.com>
2025-05-13 11:28:52 +0530

    added s3 for remotebackend to store statefile and dynamodb to maintain state lock feature

--- ecs-fargate-wordpress-rds-terraform/provider.tf
+++ ecs-fargate-wordpress-rds-terraform/provider.tf
@@ -5,8 +5,19 @@ terraform {
       version = "~> 5.40.0"
     }
   }
+
+
+  backend "s3" {
+    bucket         = "ecs-service-007"
+    key            = "terraform/ecs-fargate-wordpress-rds/terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "ecs-dynamo-007"
+    encrypt        = true
+  }
+
 }
 
 provider "aws" {
   region = var.region
 }
+
